ff69447b19f462146555f2c7250375e74c67c930
Ludwig VaGa <ludwigvagasi020898@gmail.com>
2022-10-26 15:21:47 -0500

    Se actualizo para que mostrara las listas de cursos y estudiantes

--- my_blog/course/views.py
+++ my_blog/course/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.template import loader
-
+from django.shortcuts import render
 from course.models import Course
 
 
@@ -14,3 +14,14 @@ def create_course(request, name: str = "course", code: int = 0):
     context_dict = {"course": course}
     render = template.render(context_dict)
     return HttpResponse(render)
+
+def courses(request):
+    courses = Course.objects.all()
+    
+    context_dict = {'courses': courses}
+    
+    return render(
+        request = request,
+        context = context_dict,
+        template_name= 'course/courses_list.html'
+    )
