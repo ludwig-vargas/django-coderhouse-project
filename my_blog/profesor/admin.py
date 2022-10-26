79775e2339821addb2ff7d15644526ded50994de
Ludwig VaGa <ludwigvagasi020898@gmail.com>
2022-10-26 18:40:01 -0500

    Se creo la aplicacion de profesor con sus respectivas acciones

---
+++ my_blog/profesor/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from profesor.models import Profesor
+
+# Register your models here.
+
+admin.site.register(Profesor)
---
+++ my_blog/profesor/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+# Create your views here.
+
+from profesor.models import Profesor
+
+def create_profesor(request, name: str, last_name: str, email: str, profession: str):
+
+    template = loader.get_template("template_profesor.html")
+
+    profesor = Profesor(
+        name=name, last_name=last_name, email=email, profession=profession
+    )
+    profesor.save()  # save into the DB
+
+    context_dict = {"profesor": profesor}
+    render = template.render(context_dict)
+    return HttpResponse(render)
+
+def profesors(request):
+    profesors = Profesor.objects.all()
+
+    context_dict = {"profesors": profesors}
+
+    return render(
+        request=request,
+        context=context_dict,
+        template_name="profesor/profesor_list.html")
+    
